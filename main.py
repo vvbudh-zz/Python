87a9983633a9b37cbc25124d0b48a4333782471c
vvbudh <38596508+vvbudh@users.noreply.github.com>
2019-10-25 10:43:57 -0700

    Add files via upload
    The main for my Fun program project.

---
+++ main.py
@@ -0,0 +1,29 @@
+def main:
+    """This is the flavor text for this funny.main program. Masato (I) made it a fun program
+    because I'm having a really bad day. This is in hopes that I can heal myself and become
+    a better person. I just want to make people laugh and be happy. Kinda' sad isn't it.
+    Almost sounds like the start of a bad movie."""
+
+
+class player:
+    """This is the player, the main character the PC man. Played Character.
+    He's got health, what his primary weapon's name is, his three inventory slots and
+    what location he's in."""
+    def ___init___ (self, health, primaryWeapon, inv1, inv2, inv3,location)
+
+class home:
+    def ___init___ (self, listOfUnits)
+
+
+
+
+
+
+
+class knife:
+    """"This is a knife. 1 damage"""
+    def ___init___(self, location, quality, price, damage):
+        self.location = location
+        self.quality = quality
+        self.price = price
+        self.damage = damage
